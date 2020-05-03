0b2ec330ccb074a378c21fedb66b9f49dc6cd83f
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2020-05-03 15:00:24 +0300

    Added two data members of type Data to the Product class.

--- Warehouse/Warehouse/Product.cpp
+++ Warehouse/Warehouse/Product.cpp
@@ -1,14 +1,15 @@
 #include "Product.h"
 #include <cstring>
 
-Product::Product() : name(""), manufacturerName(""), unit(""), availableQuantity(0), location(), comment("") {}
+Product::Product() : name(""), manufacturerName(""), unit(""), expiryDate(), entryDate(), availableQuantity(0), location(), comment("") {}
 
-Product::Product(const string& name, const string& manufacturerName, const string& unit, unsigned int availableQuantity, const ProductLocation& location, const string& comment) 
-				: name(name), manufacturerName(manufacturerName), unit(unit), availableQuantity(availableQuantity), location(location), comment(comment) {}
+Product::Product(const string& name, const string& manufacturerName, const string& unit, const Date& expiryDate, const Date& entryDate, 
+				unsigned int availableQuantity, const ProductLocation& location, const string& comment) 
+				: name(name), manufacturerName(manufacturerName), unit(unit), expiryDate(expiryDate), entryDate(entryDate), availableQuantity(availableQuantity), location(location), comment(comment) {}
 
 Product::Product(const Product& other) 
-				: name(other.name), manufacturerName(other.manufacturerName), unit(other.unit), availableQuantity(other.availableQuantity), 
-				  location(other.location), comment(other.comment) {}
+				: name(other.name), manufacturerName(other.manufacturerName), unit(other.unit), expiryDate(other.expiryDate), entryDate(other.entryDate), 
+				  availableQuantity(other.availableQuantity), location(other.location), comment(other.comment) {}
 
 Product& Product::operator=(const Product& other) {
 	if (this != &other)
@@ -16,6 +17,8 @@ Product& Product::operator=(const Product& other) {
 		this->name = other.name;
 		this->manufacturerName = other.manufacturerName;
 		this->unit = other.unit;
+		this->expiryDate = other.expiryDate;
+		this->entryDate = other.entryDate;
 		this->availableQuantity = other.availableQuantity;
 		this->location = other.location;
 		this->comment = other.comment;
@@ -36,6 +39,14 @@ string Product::getManufacturerName() const {
 
 string Product::getUnit() const {
 	return this->unit;
+}
+
+Date Product::getExpiryDate() const {
+	return this->expiryDate;
+}
+
+Date Product::getEntryDate() const {
+	return this->entryDate;
 }
 
 unsigned int Product::getAvailableQuantity() const {
@@ -60,6 +71,14 @@ void Product::setManufacturerName(const string& manufacturerName) {
 
 void Product::setUnit(const string& unit) {
 	this->unit = unit;
+}
+
+void Product::setExpiryDate(const Date& expiryDate) {
+	this->expiryDate = expiryDate;
+}
+
+void Product::setEntryDate(const Date& entryDate) {
+	this->entryDate = entryDate;
 }
 
 void Product::setAvailableQuantity(const unsigned int quantity) {
--- Warehouse/Warehouse/Product.h
+++ Warehouse/Warehouse/Product.h
@@ -1,23 +1,24 @@
 #pragma once
 #include <string>
 #include "ProductLocation.h"
+#include "Date.h"
 
 using namespace std;
 
 class Product  {
 private:
 	string name;
-	//Date expiryDate;
-	//Date entryDate;
 	string manufacturerName;
 	string unit;
+	Date expiryDate;
+	Date entryDate;
 	unsigned int availableQuantity;
 	ProductLocation location;
 	string comment;
 
 public:
 	Product();
-	Product(const string& name, const string& manufacturerName, const string& unit, unsigned int availableQuantity, const ProductLocation& location, const string& comment);
+	Product(const string& name, const string& manufacturerName, const string& unit, const Date& expiryDate, const Date& entryDate, unsigned int availableQuantity, const ProductLocation& location, const string& comment);
 	Product(const Product& other);
 	Product& operator=(const Product& other);
 	~Product();
@@ -25,6 +26,8 @@ public:
 	string getName() const;
 	string getManufacturerName() const;
 	string getUnit() const;
+	Date getExpiryDate() const;
+	Date getEntryDate() const;
 	unsigned int getAvailableQuantity() const;
 	ProductLocation getLocation() const;
 	string getComment() const;
@@ -32,6 +35,8 @@ public:
 	void setName(const string& name);
 	void setManufacturerName(const string& manufacturerName);
 	void setUnit(const string& unit);
+	void setExpiryDate(const Date& expiryDate);
+	void setEntryDate(const Date& entryDate);
 	void setAvailableQuantity(const unsigned int quantity);
 	void setLocation(const ProductLocation& location);
 	void setComment(const string& comment);
