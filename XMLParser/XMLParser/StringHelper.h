f45e4e439ad6557e77fcacc54bb50620b12b544b
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2020-05-14 17:46:56 +0300

    Returned the split method to the StringHelper, written without the getWord private function.

--- XMLParser/XMLParser/StringHelper.h
+++ XMLParser/XMLParser/StringHelper.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include <string>
 #include <regex>
 
@@ -6,8 +7,27 @@ using namespace std;
 
 class StringHelper {
 public:
+	static vector<string> split(const string& line);
 	static string trim(const string& line);
 };
+
+vector<string> StringHelper::split(const string& line) {
+	int lastSpaceIndex = 0;
+	int lineLength = line.size();
+
+	vector<string> tokens;
+
+	for (int i = 0; i < lineLength; i++)
+	{
+		if (line[i] == ' ') {
+			tokens.push_back(line.substr(lastSpaceIndex, i - lastSpaceIndex));
+			lastSpaceIndex = i + 1;
+		}
+	}
+	tokens.push_back(line.substr(lastSpaceIndex, lineLength - lastSpaceIndex));
+
+	return tokens;
+}
 
 string StringHelper::trim(const string& line) {
 	return regex_replace(line, std::regex("^ +"), "");
