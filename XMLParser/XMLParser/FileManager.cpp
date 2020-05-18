bba4443ed36cf6bbc9c81c7b0e376f08acc6a4a1
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2020-05-18 19:47:01 +0300

    Changed the casing of 'saveas' command.

--- XMLParser/XMLParser/FileManager.cpp
+++ XMLParser/XMLParser/FileManager.cpp
@@ -12,7 +12,7 @@ const string HELP_MESSAGE = "The following commands are supported:\n"
 							"open <file>	opens <file>\n"
 							"close		closes currently opened file\n"
 							"save		saves the currently open file\n"
-							"saveAs <file>	saves the currently open file in <file>\n"
+							"saveas <file>	saves the currently open file in <file>\n"
 							"help		prints this information\n"
 							"exit		exits the program";
 
