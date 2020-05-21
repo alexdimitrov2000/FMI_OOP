cdfa40231ce0558217465b34c6d86dfd439cf71c
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2020-05-21 19:19:06 +0300

    Implemented the logic for getting user input and executing file management commands.

--- Warehouse/Warehouse/main.cpp
+++ Warehouse/Warehouse/main.cpp
@@ -1,13 +1,72 @@
 #include <iostream>
-#include "Product.h"
-using namespace std;
+#include "Warehouse.h"
+#include "FileManager.h"
+#include "StringHelper.h"
 
-int main() 
+const char WELCOME_MESSAGE[] = "Welcome to our 'Warehouse' application! \nYou can enter 'help' for commands info.\n";
+const char USER_INPUT_MESSAGE[] = "Please, enter your command or type 'exit' if you want to quit: ";
+const char EXIT_MESSAGE[] = "Exiting the program...";
+
+// Commands
+const char OPEN_COMMAND[] = "open";
+const char SAVE_COMMAND[] = "save";
+const char SAVE_AS_COMMAND[] = "saveas";
+const char CLOSE_COMMAND[] = "close";
+const char HELP_COMMAND[] = "help";
+const char EXIT_COMMAND[] = "exit";
+
+// Command Messages
+const char CLOSE_OPENED_FILE_MESSAGE[] = "There is an opened file. Please close it before you open another one.";
+const char UNOPENED_FILE_MESSAGE[] = "There is no file opened. You have to open a file at first.";
+const char INVALID_COMMAND_MESSAGE[] = "Invalid command.";
+
+int main()
 {
-	Product prod;
-	prod.setName("Bread");
-	Product prod2(prod);
-	Product prod3 = prod2;
+	cout << WELCOME_MESSAGE << endl;
+	string userInput;
+	cout << USER_INPUT_MESSAGE;
+	getline(cin, userInput);
 
-	cout << endl;
+	FileManager fileManager;
+
+	while (userInput != EXIT_COMMAND)
+	{
+		vector<string> tokens = StringHelper::split(userInput);
+		string command = tokens[0];
+
+		if (command == OPEN_COMMAND && fileManager.isFileOpened()) {
+			cout << CLOSE_OPENED_FILE_MESSAGE << endl;
+		}
+		else if (command == OPEN_COMMAND) {
+			string filePath = tokens[1];
+			fileManager.open(filePath);
+
+			if (!fileManager.isFileOpened())
+				fileManager.isFileOpened(true);
+		}
+		else if (command == HELP_COMMAND) {
+			fileManager.help();
+		}
+		else if (!fileManager.isFileOpened()) {
+			cout << UNOPENED_FILE_MESSAGE << endl;
+		}
+		else if (command == SAVE_COMMAND) {
+			fileManager.save();
+		}
+		else if (command == SAVE_AS_COMMAND) {
+			string filePath = tokens[1];
+			fileManager.saveAs(filePath);
+		}
+		else if (command == CLOSE_COMMAND) {
+			fileManager.close();
+		}
+		else {
+			cout << INVALID_COMMAND_MESSAGE << endl;
+		}
+
+		cout << USER_INPUT_MESSAGE;
+		getline(cin, userInput);
+	}
+
+	cout << EXIT_MESSAGE << endl;
 }
